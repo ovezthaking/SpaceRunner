bb4eb1802caccfe8f0679de788f71884f79defec
OveZ <41060185+ovezthaking@users.noreply.github.com>
2023-06-16 01:39:09 +0200

    Update sprites.py

--- code/old/sprites.py
+++ code/old/sprites.py
@@ -5,7 +5,7 @@ from random import choice, randint
 class BG(pygame.sprite.Sprite):
 	def __init__(self, groups,scale_factor):
 		super().__init__(groups)
-		bg_image = pygame.image.load('C:/Users/OliwerUrbaniak/Documents/Visual Studio Code/Python/SpaceRunner/graphics/environment/background.png').convert()
+		bg_image = pygame.image.load('./SpaceRunner/graphics/environment/background.png').convert()
 
 		full_height = bg_image.get_height() * scale_factor
 		full_width = bg_image.get_width() * scale_factor
@@ -30,7 +30,7 @@ class Ground(pygame.sprite.Sprite):
 		self.sprite_type = 'ground'
 		
 		# image
-		ground_surf = pygame.image.load('C:/Users/OliwerUrbaniak/Documents/Visual Studio Code/Python/SpaceRunner/graphics/environment/ground.png').convert_alpha()
+		ground_surf = pygame.image.load('./SpaceRunner/graphics/environment/ground.png').convert_alpha()
 		self.image = pygame.transform.scale(ground_surf,pygame.math.Vector2(ground_surf.get_size()) * scale_factor)
 		
 		# position
@@ -68,13 +68,13 @@ class Ship(pygame.sprite.Sprite):
 		self.mask = pygame.mask.from_surface(self.image)
 
 		# sound
-		self.jump_sound = pygame.mixer.Sound('C:/Users/OliwerUrbaniak/Documents/Visual Studio Code/Python/SpaceRunner/sounds/jump.wav')
+		self.jump_sound = pygame.mixer.Sound('./SpaceRunner/sounds/jump.wav')
 		self.jump_sound.set_volume(0.5)
 
 	def import_frames(self,scale_factor):
 		self.frames = []
 		for i in range(3):
-			surf = pygame.image.load(f'C:/Users/OliwerUrbaniak/Documents/Visual Studio Code/Python/SpaceRunner/graphics/ship/smil{i}.png').convert_alpha()
+			surf = pygame.image.load(f'./SpaceRunner/graphics/ship/smil{i}.png').convert_alpha()
 			scaled_surface = pygame.transform.scale(surf,pygame.math.Vector2(surf.get_size())* scale_factor)
 			self.frames.append(scaled_surface)
 
@@ -109,7 +109,7 @@ class Obstacle(pygame.sprite.Sprite):
 		self.sprite_type = 'obstacle'
 
 		orientation = choice(('up','down'))
-		surf = pygame.image.load(f'C:/Users/OliwerUrbaniak/Documents/Visual Studio Code/Python/SpaceRunner/graphics/obstacles/{choice((1,2))}.png').convert_alpha()
+		surf = pygame.image.load(f'./SpaceRunner/graphics/obstacles/{choice((1,2))}.png').convert_alpha()
 		self.image = pygame.transform.scale(surf,pygame.math.Vector2(surf.get_size()) * scale_factor)
 		
 		x = WINDOW_WIDTH + randint(40,100)
